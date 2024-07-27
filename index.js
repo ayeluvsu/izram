a10c478894a9464c1f4635c8875a17ef60aebdfc
ayeluvsu <158154505+ayeluvsu@users.noreply.github.com>
2024-07-27 13:39:51 +0800

    Update index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["<a:yellowstar:1266612227113222155> DROP YOUR ORDERS NOW";
+const statusMessages = ["DROP YOUR ORDERS NOW";
 
 
 let currentIndex = 0;
